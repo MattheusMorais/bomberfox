387fe3fb47698167319f3f3f34e9ccd15f388bc6
Matheus Morais <moraism.dev@gmail.com>
2026-04-27 13:10:31 -0300

    fix: correct Main flow and remove old docs

--- Main.py
+++ Main.py
@@ -1,23 +1,18 @@
-from model.Helper import clear_screen
 from tkinter import *
 from controller.Gameplay import Gameplay
-
+ 
 def main():
     """ 
     Inicializa e executa o loop principal do jogo.
-
-    Observação: O loop while True garante que o jogador possa reiniciar o jogo sem que a execução anterior continue em segundo plano.
-
     """
     window = Tk()
     window.title("BomberBoy")
-    window.geometry("800x800")
+    window.geometry("1000x1000")
     window.minsize(400, 300)
-
+ 
     game_play = Gameplay(window)
     window.mainloop()
-    game_play.game_loop()
-
+ 
 if __name__ == "__main__":
-    clear_screen() 
     main()
+    
